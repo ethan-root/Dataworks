cf690d17429e08b111977b8cceb94c7071bc4a07
panda-p86 <56904493+panda-p86@users.noreply.github.com>
2026-02-27 11:19:13 +0800

    Update create-table.sql
    update sql

--- projects/Test/create-table.sql
+++ projects/Test/create-table.sql
@@ -1,11 +1,8 @@
-CREATE TABLE IF NOT EXISTS prod_github_test (
-    `col1`  STRING          COMMENT '*',
-    `col2`  STRING          COMMENT '*',
-    `col3`  DECIMAL(38,18)  COMMENT '*',
-    `col4`  TIMESTAMP       COMMENT '*',
-    `col5`  BIGINT          COMMENT '*',
-    `col6`  DECIMAL(38,18)  COMMENT '*'
+CREATE TABLE IF NOT EXISTS github_test_2(
+`name`                          STRING COMMENT '',
+`age`                           STRING COMMENT '',
+`location`                      STRING COMMENT ''
 )
 COMMENT 'null'
-PARTITIONED BY (pt STRING)
-LIFECYCLE 36500;
+PARTITIONED BY (pt STRING) 
+lifecycle 36500;
