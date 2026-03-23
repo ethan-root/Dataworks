084426a342c39d86a41feb2bb7d37b0da028f064
ethan <ethan.root.0x0@gmail.com>
2026-03-23 14:15:02 +0800

    feat(parameters): configure new cloud resources ending in _final_02 for pipeline testing

---
+++ features/test-feature/ddl/202603241418_create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS feature_demo_final_02(
+`name`                          STRING COMMENT '',
+`age`                           STRING COMMENT '',
+`location`                      STRING COMMENT ''
+)
+COMMENT 'null'
+PARTITIONED BY (pt STRING) 
+lifecycle 36500;
