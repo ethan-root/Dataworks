caa596ed24804a5d35981f8f3cc8be53165b8621
ethan <ethan.root.0x0@gmail.com>
2026-03-24 10:09:08 +0800

    test(regression): switch to _final_03 resource set for fresh pipeline validation

--- features/test-feature/ddl/202603241418_create.sql
+++ features/test-feature/ddl/202603241418_create.sql
@@ -1,8 +1,12 @@
-CREATE TABLE IF NOT EXISTS feature_demo_final_02(
-`name`                          STRING COMMENT '',
-`age`                           STRING COMMENT '',
-`location`                      STRING COMMENT ''
+CREATE TABLE IF NOT EXISTS feature_demo_final_03(
+    `name`                          STRING COMMENT '',
+    `age`                           STRING COMMENT '',
+    `location`                      STRING COMMENT '',
+    `id`                            STRING COMMENT '',
+    `user_name`                     STRING COMMENT '',
+    `gender`                        STRING COMMENT '',
+    `address`                       STRING COMMENT '',
+    `email`                         STRING COMMENT ''
 )
-COMMENT 'null'
-PARTITIONED BY (pt STRING) 
-lifecycle 36500;
+COMMENT 'feature_demo_final_03 regression test table'
+PARTITIONED BY (pt STRING);
